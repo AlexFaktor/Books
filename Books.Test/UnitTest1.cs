e9e9a945dc2a0475a0b2f49a1ab2c413c36eb9cf
Alex Faktor <118138704+AlexFaktor@users.noreply.github.com>
2023-12-17 12:14:05 +0200

    checkpoint

---
+++ Books.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Books.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Books/Models.cs
@@ -0,0 +1,31 @@
+﻿namespace Books
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+        public string? Title { get; set; }
+        public int Pages { get; set; }
+        public Guid GenreId { get; set; }
+        public Guid AuthorId { get; set; }
+        public Guid PublisherId { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+
+    public class Genre
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+    }
+
+    public class Author
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+    }
+
+    public class Publisher
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
